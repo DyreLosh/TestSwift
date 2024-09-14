d8210156caed64529ad681b599a6bbbe583a3ae8
DyreLosh <dyrelosh@gmail.com>
2024-09-14 19:27:27 +0300

    Initial Commit

---
+++ TestXcode/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestXcode
+//
+//  Created by Динар on 14.09.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello ")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestXcode/TestXcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestXcodeApp.swift
+//  TestXcode
+//
+//  Created by Динар on 14.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct TestXcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
